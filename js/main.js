8de1fd89812f6f62c706b53b02099040a653b131
Michael Eden <Mikey.Eden@gmail.com>
2017-04-11 00:33:00 -0400

    Made layout for the website, began filling in some details

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log("COMPILES")
+
+$(document).ready(function() {
+	$('div.project').hover(mouseEnterIcon, mouseLeaveIcon)
+})
+
+// TODO: do not have element appear so abruptly
+
+function mouseEnterIcon() {
+	$(this).children('div.project__info').show()
+	$(this).children('div.project__info').animate({ opacity: 1 }, 200)
+    $(this).children('img.project__image').animate({ opacity: 0.7 }, 200);
+}
+
+function mouseLeaveIcon() {
+	$(this).children('div.project__info').animate({ opacity: 0 }, 200)
+    $(this).children('img.project__image').animate({ opacity: 1 }, 200);
+}
