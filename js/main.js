cfa6435887b0e99a13b4d4f3449312a1565cb1f2
Michael Eden <Mikey.Eden@gmail.com>
2017-04-12 23:27:49 -0400

    Added a few sections, some work on projects carousel

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
-console.log("COMPILES")
-
 $(document).ready(function() {
-	$('div.project').hover(mouseEnterIcon, mouseLeaveIcon)
+	$('.carousel').carousel({ interval: false });
+	$('div.project').hover(mouseEnterProject, mouseLeaveProject);
 })
+
+$('.multi-item-carousel .item').each(function() {
+	conjoinSlides($(this), $(this), 3)
+})
+
+// Recursively adds slides to each other to create a multi-item carousel
+function conjoinSlides(slide, slide_added, slides) {
+	if (slides <= 1) {
+		return;
+	}
+
+	// Get the next sibling, and check if it exists
+	slide_added = slide_added.next('.item');
+	if (!slide_added.length) {
+		slide_added = slide.siblings('.item:first');
+	}
+	slide_added.children(':first-child').clone().appendTo(slide);
+	conjoinSlides(slide, slide_added, slides - 1);
+}
 
 // TODO: do not have element appear so abruptly
 
-function mouseEnterIcon() {
+function mouseEnterProject() {
 	$(this).children('div.project__info').show()
 	$(this).children('div.project__info').animate({ opacity: 1 }, 200)
     $(this).children('img.project__image').animate({ opacity: 0.4 }, 200);
 }
 
-function mouseLeaveIcon() {
+function mouseLeaveProject() {
 	$(this).children('div.project__info').animate({ opacity: 0 }, 200)
     $(this).children('img.project__image').animate({ opacity: 1 }, 200);
 }
