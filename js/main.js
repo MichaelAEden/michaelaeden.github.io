b97e6899e7a5a89436a8d96e5174decdb5e9013c
Michael Eden <Mikey.Eden@gmail.com>
2017-05-16 00:57:45 -0400

    Added navbar, working project slider, other small improvements

--- js/main.js
+++ js/main.js
@@ -1,26 +1,9 @@
 $(document).ready(function() {
-	$('.carousel').carousel({ interval: false });
-	$('div.project').hover(mouseEnterProject, mouseLeaveProject);
+	$('li.project').hover(mouseEnterProject, mouseLeaveProject);
+	$('#projects__slider').lightSlider({
+		 item: 3 
+	});
 })
-
-$('.multi-item-carousel .item').each(function() {
-	conjoinSlides($(this), $(this), 3)
-})
-
-// Recursively adds slides to each other to create a multi-item carousel
-function conjoinSlides(slide, slide_added, slides) {
-	if (slides <= 1) {
-		return;
-	}
-
-	// Get the next sibling, and check if it exists
-	slide_added = slide_added.next('.item');
-	if (!slide_added.length) {
-		slide_added = slide.siblings('.item:first');
-	}
-	slide_added.children(':first-child').clone().appendTo(slide);
-	conjoinSlides(slide, slide_added, slides - 1);
-}
 
 // TODO: do not have element appear so abruptly
 
